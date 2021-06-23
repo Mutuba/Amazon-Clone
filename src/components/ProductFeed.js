05f1137d8e2745c0cf0bf9b9af2710dcb462810c
Mutuba <danielmutubait@gmail.com>
2021-06-23 21:44:23 +0300

    fix typos

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,14 +3,14 @@ import Product from "./Product";
 
 const ProductFeed = ({ products }) => {
   return (
-
     // "dense" packing algorithm attempts to fill in holes earlier in the grid,
     //  if smaller items come up later. This may cause items to appear out-of-order, when doing so would fill in holes left by larger items.
 
     // column-Items are placed by filling each column in turn, adding new columns as necessary.
     //row - Items are placed by filling each row in turn, adding new rows as necessary
+
     <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
-      {products.map(({id, title, price, description, category, image}) => (
+      {products.map(({ id, title, price, description, category, image }) => (
         <Product
           key={id}
           title={title}
